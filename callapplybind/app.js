2fac2d25e5e6f727e75c83ada4d78d5009440caa
Raghu <konidelaraghu@gmail.com>
2020-09-10 22:01:30 +0100

    Added Definitions for Call, apply and Bind

--- callapplybind/app.js
+++ callapplybind/app.js
@@ -12,10 +12,16 @@ var logName = function (lang1, lang2) {
   console.log('Arguments ---- ' + lang1 + ' ' + lang2);
   console.log('---------');
 };
+
+//Bind creates a copy of whatever function you're calling it on.
 var logPersonName = logName.bind(person);
 
 logPersonName('ENGLISH', 'VINGLISH');
 
+// call() executes the function and decides what the this variable should be and the rest
+// is just the parameters that would normally pass to a function.
+
 logName.call(person, 'Eng', 'British');
 
+// Does the same as above, but the only difference is parameters to be passed in an Array format.
 logName.apply(person, ['Eng', 'Brit']);
