4c43ae4ed92579d375a6ba4420aca05a1cb64f1c
Raghu <konidelaraghu@gmail.com>
2020-09-10 22:20:09 +0100

    real life examples for call, apply, bind

--- callapplybind/app.js
+++ callapplybind/app.js
@@ -25,3 +25,31 @@ logName.call(person, 'Eng', 'British');
 
 // Does the same as above, but the only difference is parameters to be passed in an Array format.
 logName.apply(person, ['Eng', 'Brit']);
+
+// Real life instances -
+
+// Function Borrowing
+var person1 = {
+  firstName: 'Siddu',
+  lastName: 'Reddy',
+};
+
+console.log(person.getFullname.apply(person1));
+
+// Function currying
+function multiply(a, b) {
+  return a * b;
+}
+
+var multiplyByTwo = multiply.bind(this, 2);
+// Giving the parameters to Bind sets the permanant values when the copy is made.
+// i.e., variable a is always 2 in the copy of the function.
+// this means
+// function multiplyByTwo(b) {
+//   var a = 2;
+//   return a * b;
+// }
+console.log(multiplyByTwo(4));
+
+// Function Currying -
+// Creating a copy of a function but with some preset parameters
