7a6dec16938b6b88f0447da91ac76357be23e382
Raghu <konidelaraghu@gmail.com>
2020-09-21 23:23:54 +0100

    Example with framework

--- MyFramework/app.js
+++ MyFramework/app.js
@@ -9,3 +9,13 @@ g.greet(); // This will print normal Greeting!
 g.greet().greet(true); // This will print normal Greeting and Formal Greeting
 
 g.greet().setLang('es').greet(true); // This will print normal Greeting and Formal Greeting in Spanish!
+
+// Using  our framework
+
+$('#login').click(function () {
+  var loginGreeter = G$('Raghu', 'Reddy');
+  //hides on click of login
+  $('#logindiv').hide();
+  // Print the desired output in console
+  loginGreeter.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+});
